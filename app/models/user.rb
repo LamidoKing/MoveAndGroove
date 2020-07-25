83aa88b8cb5d5f2b93280b700e82e36a36affe75
lamido <kinglamido@gmail.com>
2020-07-25 15:24:06 +0100

    ft:add user model, registration and signin pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
